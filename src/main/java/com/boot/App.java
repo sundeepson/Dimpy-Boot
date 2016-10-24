d4ad605967101dc96eeeeebef2723219d0320cd2
Maddela Dinakara sandeep <sundeepson@gmail.com>
2016-10-24 21:49:45 +0530

    Spring MVC

---
+++ src/main/java/com/boot/App.java
@@ -0,0 +1,22 @@
+package com.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+public class App 
+
+
+{
+    
+	public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
